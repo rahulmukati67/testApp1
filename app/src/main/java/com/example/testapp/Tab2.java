6dc5f7ed85b870a968f3caec69d478980b5c6f94
anikety106 <anikety106@gmail.com>
2024-02-09 15:27:44 +0530

    webView2

--- app/src/main/java/com/example/testapp/Tab2.java
+++ app/src/main/java/com/example/testapp/Tab2.java
@@ -1,64 +1,46 @@
 package com.example.testapp;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Tab2#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Tab2 extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Tab2() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Tab2.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Tab2 newInstance(String param1, String param2) {
-        Tab2 fragment = new Tab2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_view, container, false);
+        return inflater.inflate(R.layout.fragment_tab2, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView Date1 = view.findViewById(R.id.edittxtDate1);
+        TextView Date2 = view.findViewById(R.id.edittxtDate2);
+
+        Date date = new Date();
+        LocalDateTime localDateTime = null;
+        String formattedDate = null;
+        DateTimeFormatter formatter = null;
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            localDateTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+            formattedDate = localDateTime.format(formatter);
+        } else{
+            SimpleDateFormat sdf = new SimpleDateFormat("MMMM dd, yyyy");
+             formattedDate = sdf.format(date);
+        }
+        Date1.setText(formattedDate);
+        Date2.setText(formattedDate);
     }
 }
--- app/src/main/java/com/example/testapp/Tab3.java
+++ app/src/main/java/com/example/testapp/Tab3.java
@@ -59,6 +59,6 @@ public class Tab3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_view, container, false);
+        return inflater.inflate(R.layout.fragment_tab3, container, false);
     }
 }
